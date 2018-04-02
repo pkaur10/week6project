882d84af19e4d304a54381c417a575e9dea1de65
Pawan G <Pawan@Pawaninders-MacBook-Pro.local>
2018-04-01 19:21:31 -0700

    week6 project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function sayHi(){
+	var txtName = document.getElementByID("txtName");
+	var txtOutput = document.getElementById("txtOutput");
+
+
+}
+
+var name = txtName.value;
+txtOutput.value = Hi there, + " Firstname" + " Lastname" + , your birthdate is + " birthmonth" + " birthday" + "!"; 
